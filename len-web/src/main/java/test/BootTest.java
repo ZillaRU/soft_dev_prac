ef9e20bc51a01224e70f3f5ee75c2f9b519cfeed
zxm <154040976@qq.com>
2018-01-29 10:04:44 +0800

    1

--- len-web/src/main/java/test/BootTest.java
+++ len-web/src/main/java/test/BootTest.java
@@ -25,7 +25,7 @@ public class BootTest {
 
   @Test
   public void testStartJob() throws Exception {
-    //1
+    //12
     SysUser user=userService.selectByPrimaryKey("2211fec3e17c11e795ed201a068c6482");
     System.out.println(user.getUsername());
   }
