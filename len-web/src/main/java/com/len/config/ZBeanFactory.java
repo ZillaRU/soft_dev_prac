64654630f8ca47c968ef88b6ccedffe2605ce783
zxm <154040976@qq.com>
2018-05-01 17:42:14 +0800

    升级 log4j到 log4j2 解决boot日志兼容问题

--- len-web/src/main/java/com/len/config/ZBeanFactory.java
+++ len-web/src/main/java/com/len/config/ZBeanFactory.java
@@ -1,9 +1,7 @@
 package com.len.config;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * @author zhuxiaomeng
  * @date 2018/1/4.
  * @email 154040976@qq.com
- *
+ * <p>
  * ApplicationContext会自动检查是否在定义文件中有实现了BeanPostProcessor接口的类，
  * 如果有的话，Spring容器会在每个Bean(其他的Bean)被初始化之前和初始化之后，
  * 分别调用实现了BeanPostProcessor接口的类的postProcessAfterInitialization()方法
@@ -22,17 +20,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ZBeanFactory implements BeanPostProcessor {
 
-private static final Logger log=LoggerFactory.getLogger(ZBeanFactory.class);
-  @Override
-  public Object postProcessBeforeInitialization(Object bean, String beanName)
-      throws BeansException {
-    log.info("对象---"+beanName+"---初始化开始");
-    return bean;
-  }
+    private static final Logger log = LogManager.getLogger(ZBeanFactory.class);
 
-  @Override
-  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-    log.info("对象---"+beanName+"---初始化成功");
-    return bean;
-  }
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        log.info("对象---" + beanName + "---初始化开始");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("对象---" + beanName + "---初始化成功");
+        return bean;
+    }
 }
