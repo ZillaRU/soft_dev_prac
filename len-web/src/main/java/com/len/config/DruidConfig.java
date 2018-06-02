73a5a3e3bad99757a945cad4bd2355eaf16f61ed
zxm <154040976@qq.com>
2018-06-02 12:48:00 +0800

    更改日志 设为logback，修复错误不打印bug

--- len-web/src/main/java/com/len/config/DruidConfig.java
+++ len-web/src/main/java/com/len/config/DruidConfig.java
@@ -4,13 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.sql.DataSource;
 import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -22,6 +15,11 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zhuxiaomeng
  * @date 2018/1/2.
@@ -31,65 +29,65 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 @EnableTransactionManagement
 public class DruidConfig {
 
-  @Value("${spring.datasource.url}")
-  private String url;
-  @Value("${spring.datasource.username}")
-  private String username;
-  @Value("${spring.datasource.password}")
-  private String password;
-  @Value("${spring.datasource.filters}")
-  private String filters;
-  @Value("${spring.datasource.driver-class-name}")
-  private String driverClassName;
-  @Value("${spring.datasource.initialSize}")
-  private int initialSize;
-  @Value("${spring.datasource.minIdle}")
-  private int minIdle;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.filters}")
+    private String filters;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
 
-  @Bean
-  @Primary
-  public DataSource getDataSource(){
-    DruidDataSource datasource = new DruidDataSource();
+    @Bean
+    @Primary
+    public DataSource getDataSource() {
+        DruidDataSource datasource = new DruidDataSource();
 
-    datasource.setUrl(url);
-    datasource.setUsername(username);
-    datasource.setPassword(password);
-    datasource.setDriverClassName(driverClassName);
-    datasource.setInitialSize(initialSize);
-    datasource.setMinIdle(minIdle);
-    try {
-      datasource.setFilters(filters);
-    } catch (SQLException e) {
-      e.printStackTrace();
+        datasource.setUrl(url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return datasource;
     }
-    return datasource;
-  }
 
-  @Bean
-  public FilterRegistrationBean filterRegistrationBean() {
-    FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-    filterRegistrationBean.setFilter(new WebStatFilter());
-    filterRegistrationBean.addUrlPatterns("/*");
-    filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*,*.html");
-    DelegatingFilterProxy proxy = new DelegatingFilterProxy();
-    proxy.setTargetFilterLifecycle(true);
-    proxy.setTargetBeanName("shiroFilter");
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*,*.html");
+        DelegatingFilterProxy proxy = new DelegatingFilterProxy();
+        proxy.setTargetFilterLifecycle(true);
+        proxy.setTargetBeanName("shiroFilter");
 
-    filterRegistrationBean.setFilter(proxy);
-    return filterRegistrationBean;
-  }
+        filterRegistrationBean.setFilter(proxy);
+        return filterRegistrationBean;
+    }
 
-  @Bean
-  public ServletRegistrationBean druidServlet() {
-    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-    servletRegistrationBean.setServlet(new StatViewServlet());
-    servletRegistrationBean.addUrlMappings("/druid/*");
-    Map<String, String> initParameters = new HashMap<String, String>();
-    initParameters.put("resetEnable", "false");
-    initParameters.put("allow", "");
-    servletRegistrationBean.setInitParameters(initParameters);
-    return servletRegistrationBean;
-  }
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<String, String>();
+        initParameters.put("resetEnable", "false");
+        initParameters.put("allow", "");
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
 
   @Bean
   public DruidStatInterceptor getDruidStatInterceptor(){
