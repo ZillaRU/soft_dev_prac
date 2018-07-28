72028a388339260cf9d95978ec61b21a9bae1938
zxm <154040976@qq.com>
2018-07-29 00:01:39 +0800

    单独分出ui，后端不做变化

--- len-core/src/main/java/com/len/util/JWTUtil.java
+++ len-core/src/main/java/com/len/util/JWTUtil.java
@@ -6,17 +6,17 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 public class JWTUtil {
 
     // 过期时间5分钟
-    private static final long EXPIRE_TIME = 5*60*1000;
+    private static final long EXPIRE_TIME = 5 * 60 * 1000;
 
     /**
      * 校验token是否正确
-     * @param token 密钥
+     *
+     * @param token  密钥
      * @param secret 用户的密码
      * @return 是否正确
      */
@@ -35,6 +35,7 @@ public class JWTUtil {
 
     /**
      * 获得token中的信息无需secret解密也能获得
+     *
      * @return token中包含的用户名
      */
     public static String getUsername(String token) {
@@ -48,12 +49,13 @@ public class JWTUtil {
 
     /**
      * 生成签名,5min后过期
+     *
      * @param username 用户名
-     * @param secret 用户的密码
+     * @param secret   用户的密码
      * @return 加密的token
      */
     public static String sign(String username, String secret) {
-        Date date = new Date(System.currentTimeMillis()+EXPIRE_TIME);
+        Date date = new Date(System.currentTimeMillis() + EXPIRE_TIME);
         Algorithm algorithm = Algorithm.HMAC256(secret);
         // 附带username信息
         return JWT.create()
