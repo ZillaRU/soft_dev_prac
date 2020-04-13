a05b429c2fb7a8b861d405b91c6c9551635639d4
zilla_ru <10152140148@stu.ecnu.edu.cn>
2020-04-13 21:36:10 +0800

    fix 火狐抽风

--- len-activiti/src/main/java/com/len/entity/ProjectFunction.java
+++ len-activiti/src/main/java/com/len/entity/ProjectFunction.java
@@ -1,16 +1,28 @@
 package com.len.entity;
 
-import com.excel.poi.annotation.ExportField;
-import com.excel.poi.annotation.ImportField;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Table(name = "proj_func")
 @Data
 public class ProjectFunction {
-    @ExportField(columnName = "功能编号")
-    @ImportField(required = true, regex = "[0-9]{4}[0-9]{4}", regexMessage = "功能编号格式：一级4位 二级4位")
+    @Id
+    @Column(name = "proj_id")
+    private String projId;
+
+    @Column(name = "func_id")
     private String funcId; // pattern: 一级4位二级4位 00230045 二级全0为一级功能名称
 
-    @ExportField(columnName = "功能名称")
-    @ImportField(required = true)
+    @Column(name = "func_name")
     private String funcName;
+
+    public ProjectFunction(String projId, String funcId, String funcName) {
+        this.projId =  projId;
+        this.funcId = funcId;
+        this.funcName = funcName;
+    }
 }
---
+++ len-activiti/src/main/java/com/len/service/ProjFuncService.java
@@ -0,0 +1,10 @@
+package com.len.service;
+
+import com.len.base.BaseService;
+import com.len.entity.ProjectFunction;
+
+import java.util.List;
+
+public interface ProjFuncService extends BaseService<ProjectFunction, String> {
+    List<ProjectFunction> selectByProjId(String proj_id);
+}
